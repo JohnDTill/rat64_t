5990bc617e3cb7e322439a745249fed2d5b298d5
John Till <JohnDTill@gmail.com>
2021-05-04 16:57:16 -0500

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,198 @@
+#include <iostream>
+
+#include "rat64_t.h"
+
+using namespace std;
+
+int main(){
+    constexpr rat64_t::SignedHalfWord max_n = std::numeric_limits<int32_t>::max();
+    constexpr rat64_t::SignedHalfWord min_n = std::numeric_limits<int32_t>::min();
+    constexpr rat64_t::UnsignedHalfWord max_d = std::numeric_limits<uint32_t>::max();
+
+    rat64_t ans;
+
+    assert( rat64_t(4,2) == rat64_t({2,1}) );
+    assert( rat64_t(2,4) == rat64_t({1,2}) );
+    assert( rat64_t(6,8) == rat64_t({3,4}) );
+
+    assert( !rat64_t::multiply( rat64_t({1,2}), rat64_t({1,2}), ans ) );
+    assert( ans == rat64_t({1,4}) );
+    assert( !rat64_t::multiply( rat64_t({-1,2}), rat64_t({1,2}), ans ) );
+    assert( ans == rat64_t({-1,4}) );
+    assert( !rat64_t::multiply( rat64_t({-1,2}), rat64_t({-1,2}), ans ) );
+    assert( ans == rat64_t({1,4}) );
+    assert( !rat64_t::multiply( rat64_t({1,2}), rat64_t({2,3}), ans ) );
+    assert( ans == rat64_t({1,3}) );
+    assert( !rat64_t::multiply( rat64_t({-1,2}), rat64_t({2,3}), ans ) );
+    assert( ans == rat64_t({-1,3}) );
+    assert( !rat64_t::multiply( rat64_t({max_n,2}), rat64_t({2,1}), ans ) );
+    assert( ans == rat64_t({max_n,1}) );
+    assert( !rat64_t::multiply( rat64_t({min_n,2}), rat64_t({2,1}), ans ) );
+    assert( ans == rat64_t({min_n,1}) );
+    assert( !rat64_t::multiply( rat64_t({2,max_d}), rat64_t({1,2}), ans ) );
+    assert( ans == rat64_t({1,max_d}) );
+    assert( !rat64_t::multiply( rat64_t({max_n,1}), rat64_t({1,(uint32_t)max_n}), ans ) );
+    assert( ans == rat64_t({1,1}) );
+    assert( !rat64_t::multiply( rat64_t({min_n,1}), rat64_t({1,(uint32_t)-(int64_t)min_n}), ans ) );
+    assert( ans == rat64_t({-1,1}) );
+
+    assert( !rat64_t::multiply( rat64_t({1,2}), 3, ans ) );
+    assert( ans == rat64_t({3,2}) );
+    assert( !rat64_t::multiply( 3, rat64_t({1,2}), ans ) );
+    assert( ans == rat64_t({3,2}) );
+    assert( !rat64_t::multiply( rat64_t({1,2}), -3, ans ) );
+    assert( ans == rat64_t({-3,2}) );
+    assert( !rat64_t::multiply( -3, rat64_t({1,2}), ans ) );
+    assert( ans == rat64_t({-3,2}) );
+
+    assert( rat64_t::multiply( rat64_t({max_n,1}), rat64_t({2,1}), ans ) );
+    assert( rat64_t::multiply( rat64_t({min_n,1}), rat64_t({2,1}), ans ) );
+    assert( rat64_t::multiply( rat64_t({min_n,1}), rat64_t({-1,1}), ans ) );
+    assert( rat64_t::multiply( rat64_t({1,max_d}), rat64_t({1,2}), ans ) );
+
+    assert( !rat64_t::add( rat64_t({1,2}), rat64_t({1,2}), ans ) );
+    assert( ans == rat64_t({1,1}) );
+    assert( !rat64_t::add( rat64_t({1,2}), rat64_t({1,3}), ans ) );
+    assert( ans == rat64_t({5,6}) );
+    assert( !rat64_t::add( rat64_t({1,2}), rat64_t({-1,3}), ans ) );
+    assert( ans == rat64_t({1,6}) );
+    assert( !rat64_t::add( rat64_t({5,3}), rat64_t({-9,10}), ans ) );
+    assert( ans == rat64_t({23,30}) );
+    assert( !rat64_t::add( rat64_t({-1,2}), rat64_t({-1,2}), ans ) );
+    assert( ans == rat64_t({-1,1}) );
+    assert( !rat64_t::add( rat64_t({-1,2}), 1, ans ) );
+    assert( ans == rat64_t({1,2}) );
+    assert( !rat64_t::add( rat64_t({-1,2}), -1, ans ) );
+    assert( ans == rat64_t({-3,2}) );
+
+    assert( !rat64_t::power( rat64_t({1,2}), 2, ans ) );
+    assert( ans == rat64_t({1,4}) );
+    assert( !rat64_t::power( rat64_t({1,2}), 4, ans ) );
+    assert( ans == rat64_t({1,16}) );
+    assert( rat64_t::power( rat64_t({1,2}), 32, ans ) );
+
+    assert( rat64_t({1,4}) < rat64_t({1,3}) );
+    assert( !(rat64_t({1,3}) < rat64_t({1,3})) );
+    assert( !(rat64_t({1,4}) > rat64_t({1,3})) );
+    assert( rat64_t({1,4}) <= rat64_t({1,3}) );
+    assert( rat64_t({1,3}) <= rat64_t({1,3}) );
+
+    assert( std::to_string(static_cast<double>(rat64_t{1,3})) == "0.333333" );
+
+    assert( !rat64_t({31415926,10000000}).greaterThanPi() );
+    assert( rat64_t({31415927,10000000}).greaterThanPi() );
+
+    ans = rat64_t({1,2});
+    assert( rat64_t(static_cast<void*>(ans)) == ans );
+    ans = rat64_t({max_n, max_d});
+    assert( rat64_t(static_cast<void*>(ans)) == ans );
+
+    return 0;
+}
+
+
+/*
+#include <gmpxx.h>
+#include <math.h>
+void boundPi(){
+    std::string very_good_approximation = "3.1415926535897932384626433832795028841971693993751058"
+                                          "209749445923078164062862089986280348253421170679821480"
+                                          "865132823066470938446095505822317253594081284811174502"
+                                          "841027019385211055596446229489549303819644288109756659"
+                                          "334461284756482337867831652712019091456485669234603486"
+                                          "104543266482133936072602491412737245870066063155881748"
+                                          "815209209628292540917153643678925903600113305305488204"
+                                          "665213841469519415116094330572703657595919530921861173"
+                                          "819326117931051185480744623799627495673518857527248912"
+                                          "279381830119491298336733624406566430860213949463952247"
+                                          "371907021798609437027705392171762931767523846748184676"
+                                          "694051320005681271452635608277857713427577896091736371"
+                                          "787214684409012249534301465495853710507922796892589235"
+                                          "420199561121290219608640344181598136297747713099605187"
+                                          "072113499999983729780499510597317328160963185950244594"
+                                          "553469083026425223082533446850352619311881710100031378"
+                                          "387528865875332083814206171776691473035982534904287554"
+                                          "687311595628638823537875937519577818577805321712268066"
+                                          "130019278766111959092164201989380952572010654858632788"
+                                          "659361533818279682303019520353018529689957736225994138"
+                                          "912497217752834791315155748572424541506959508295331168"
+                                          "617278558890750983817546374649393192550604009277016711"
+                                          "390098488240128583616035637076601047101819429555961989"
+                                          "467678374494482553797747268471040475346462080466842590"
+                                          "694912933136770289891521047521620569660240580381501935"
+                                          "112533824300355876402474964732639141992726042699227967"
+                                          "823547816360093417216412199245863150302861829745557067"
+                                          "498385054945885869269956909272107975093029553211653449"
+                                          "872027559602364806654991198818347977535663698074265425"
+                                          "278625518184175746728909777727938000816470600161452491"
+                                          "921732172147723501414419735685481613611573525521334757"
+                                          "418494684385233239073941433345477624168625189835694855"
+                                          "620992192221842725502542568876717904946016534668049886"
+                                          "272327917860857843838279679766814541009538837863609506"
+                                          "800642251252051173929848960841284886269456042419652850"
+                                          "222106611863067442786220391949450471237137869609563643"
+                                          "719172874677646575739624138908658326459958133904780275"
+                                          "900994657640789512694683983525957098258226205224894077"
+                                          "267194782684826014769909026401363944374553050682034962"
+                                          "524517493996514314298091906592509372216964615157098583"
+                                          "874105978859597729754989301617539284681382686838689427"
+                                          "741559918559252459539594310499725246808459872736446958"
+                                          "486538367362226260991246080512438843904512441365497627"
+                                          "807977156914359977001296160894416948685558484063534220"
+                                          "722258284886481584560285060168427394522674676788952521"
+                                          "385225499546667278239864565961163548862305774564980355"
+                                          "936345681743241125150760694794510965960940252288797108"
+                                          "931456691368672287489405601015033086179286809208747609"
+                                          "178249385890097149096759852613655497818931297848216829";
+    for(size_t i = 1; i < very_good_approximation.size()-1; i++)
+        very_good_approximation[i] = very_good_approximation[i+1];
+    very_good_approximation.pop_back();
+    size_t decimals = very_good_approximation.size()-1;
+    very_good_approximation += "/1";
+    for(size_t i = 0; i < decimals; i++) very_good_approximation += "0";
+
+    mpq_class pi(very_good_approximation);
+
+    rat64_t low;
+    rat64_t high;
+    mpq_class low_error(-1);
+    mpq_class high_error(1);
+
+    for(uint64_t den = 1; den < std::numeric_limits<int32_t>::max(); den++){
+        int64_t start = std::min((den*314159265358)/100000000000, (uint64_t)std::numeric_limits<int32_t>::max());
+        if(start > std::numeric_limits<int32_t>::max()) break;
+        int64_t end = std::min((den*314159265359)/100000000000, (uint64_t)std::numeric_limits<int32_t>::max());
+        for(int64_t num = start; num <= end; num++){
+            if( std::abs(static_cast<double>(num)/den - 3.14159265358979) > 1e-11 ) continue;
+
+            mpq_class candidate(num, den);
+            mpq_class error = candidate - pi;
+            if(error > 0 && error < high_error){
+                high_error = error;
+                high = rat64_t(num, den);
+            }else if(error < 0 && error > low_error){
+                low_error = error;
+                low = rat64_t(num, den);
+            }
+        }
+    }
+
+    std::cout << "The closest rat64_t greater than pi is " << high
+              << " with an error of ~" << high_error.get_d()
+              << "\nThe closest rat64_t less than pi is " << low
+              << " with an error of ~" << -low_error.get_d() << std::endl;
+
+    const double pi_double = 4*atan(1.0);
+    std::string pi_double_str = std::to_string(pi_double);
+    for(size_t i = 1; i < pi_double_str.size()-1; i++)
+        pi_double_str[i] = pi_double_str[i+1];
+    pi_double_str.pop_back();
+    decimals = pi_double_str.size()-1;
+    pi_double_str += "/1";
+    for(size_t i = 0; i < decimals; i++) pi_double_str += "0";
+
+    mpq_class err = mpq_class(pi_double_str) - pi;
+
+    std::cout << "Compare with π-4*atan(1) with an error of ~" << err.get_d() << std::endl;
+}
+*/
---
+++ rat64_t.h
@@ -0,0 +1,326 @@
+//When you have a word-size union which includes a rational number (e.g. mpq_class*),
+//it can be an advantage to also have a word-size rational number. This way
+//dynamic allocation can be avoided until necessary. Most operations must be checked
+//to determine if they overflow/underflow, in which case they will return true.
+//Fractions are simplified prior to division to avoid intermediate overflow.
+
+#ifndef RAT64_T_H
+#define RAT64_T_H
+
+#include <algorithm>
+#include <assert.h>
+#include <cstring>
+#include <inttypes.h>
+#include <iostream>
+#include <numeric>
+
+struct rat64_t{
+    typedef int32_t SignedHalfWord;
+    typedef uint32_t UnsignedHalfWord;
+    typedef int64_t SignedWord;
+    typedef uint64_t UnsignedWord;
+
+    //TODO: This code calls abs() without considering abs(std::numeric_limits<SignedHalfWord>::min()) is UB
+    //      probably should limit range so num == std::numeric_limits<SignedHalfWord>::min() is an "underflow"
+    //      or figure out how to cast to UnsignedHalfWord
+
+    static UnsignedHalfWord safeAbs(const SignedHalfWord& num){
+        return std::abs(static_cast<SignedWord>(num));
+    }
+
+    static bool multWithOverflowCheck(UnsignedHalfWord a, UnsignedHalfWord b, UnsignedHalfWord& ans){
+        UnsignedWord full = static_cast<UnsignedWord>(a) * static_cast<UnsignedWord>(b);
+        ans = a*b;
+        return full > std::numeric_limits<UnsignedHalfWord>::max();
+    }
+
+    static bool multWithOverflowCheck(SignedHalfWord a, SignedHalfWord b, SignedHalfWord& ans){
+        SignedWord full = static_cast<SignedWord>(a) * static_cast<SignedWord>(b);
+        ans = full;
+        return full > std::numeric_limits<SignedHalfWord>::max() ||
+               full < std::numeric_limits<SignedHalfWord>::min();
+    }
+
+    static bool multWithOverflowCheck(SignedHalfWord a, UnsignedHalfWord b, SignedHalfWord& ans){
+        SignedWord full = static_cast<SignedWord>(a) * static_cast<SignedWord>(b);
+        ans = full;
+        return full > std::numeric_limits<SignedHalfWord>::max() ||
+               full < std::numeric_limits<SignedHalfWord>::min();
+    }
+
+    static bool multWithOverflowCheck(UnsignedHalfWord a, SignedWord b, SignedHalfWord& ans){
+        SignedWord full = static_cast<SignedWord>(a) * static_cast<SignedWord>(b);
+        ans = full;
+        return full > std::numeric_limits<SignedHalfWord>::max() ||
+               full < std::numeric_limits<SignedHalfWord>::min();
+    }
+
+    SignedHalfWord num;
+    UnsignedHalfWord den;
+
+    rat64_t() : num(0), den(1) {}
+
+    rat64_t(SignedHalfWord num) : num(num), den(1) {}
+
+    rat64_t(SignedHalfWord num, UnsignedHalfWord den){
+        assert(den!=0);
+        const auto gcd = std::gcd(safeAbs(num), den);
+        this->num = num / static_cast<int32_t>(gcd);
+        this->den = den / gcd;
+    }
+
+    rat64_t(void* vpointer){
+        memcpy(this, &vpointer, sizeof(rat64_t));
+        assert(den!=0);
+    }
+
+    operator void*() const{
+        assert(sizeof(void*) >= sizeof(rat64_t));
+        void* vpointer;
+        memcpy(&vpointer, this, sizeof(rat64_t));
+        return vpointer;
+    }
+
+    operator double() const{
+        return num / static_cast<double>(den);
+    }
+
+    static bool multiply(const rat64_t& lhs, const rat64_t& rhs, rat64_t& ans){
+        const auto gcd1 = std::gcd(safeAbs(lhs.num), rhs.den);
+        const auto gcd2 = std::gcd(safeAbs(rhs.num), lhs.den);
+
+        const SignedHalfWord n1 = lhs.num/static_cast<SignedWord>(gcd1);
+        const SignedHalfWord n2 = rhs.num/static_cast<SignedWord>(gcd2);
+        const UnsignedHalfWord d1 = lhs.den/gcd2;
+        const UnsignedHalfWord d2 = rhs.den/gcd1;
+
+        return multWithOverflowCheck(d1, d2, ans.den) || multWithOverflowCheck(n1, n2, ans.num);
+    }
+
+    static bool divide(const rat64_t& lhs, const rat64_t& rhs, rat64_t& ans){
+        const auto gcd1 = std::gcd(lhs.num, rhs.num);
+        const auto gcd2 = std::gcd(rhs.den, lhs.den);
+
+        const SignedHalfWord n1 = lhs.num/static_cast<SignedHalfWord>(gcd1);
+        const UnsignedHalfWord n2 = rhs.den/static_cast<SignedHalfWord>(gcd2);
+        const UnsignedHalfWord d1 = lhs.den/gcd2;
+        const SignedHalfWord d2 = rhs.num/gcd1;
+
+        return multWithOverflowCheck(n1, n2, ans.num) || multWithOverflowCheck(d1, d2, ans.den);
+    }
+
+    static bool multiply(const rat64_t& lhs, const UnsignedHalfWord& rhs, rat64_t& ans){
+        const auto gcd = std::gcd(lhs.den, rhs);
+        ans.den = lhs.den / gcd;
+        return multWithOverflowCheck(lhs.num, rhs/gcd, ans.num);
+    }
+
+    inline static bool multiply(const UnsignedHalfWord& lhs, const rat64_t& rhs, rat64_t& ans){
+        return multiply(rhs, lhs, ans);
+    }
+
+    static bool multiply(const rat64_t& lhs, const SignedHalfWord& rhs, rat64_t& ans){
+        const auto gcd = std::gcd(lhs.den, safeAbs(rhs));
+        ans.den = lhs.den / gcd;
+        return multWithOverflowCheck(lhs.num, rhs/static_cast<SignedHalfWord>(gcd), ans.num);
+    }
+
+    inline static bool multiply(const SignedHalfWord& lhs, const rat64_t&rhs, rat64_t& ans){
+        return multiply(rhs, lhs, ans);
+    }
+
+    static bool divide(const rat64_t& lhs, const UnsignedHalfWord& rhs, rat64_t& ans){
+        const auto gcd = std::gcd(safeAbs(lhs.num), rhs);
+        ans.num = lhs.num / gcd;
+        return multWithOverflowCheck(lhs.den, rhs/gcd, ans.den);
+    }
+
+    static bool multiply(const rat64_t& lhs, const size_t& rhs, rat64_t& ans){
+        const auto gcd = std::gcd(lhs.den, rhs);
+        ans.den = lhs.den / gcd;
+        const auto reduced_rhs = rhs/gcd;
+        return reduced_rhs > std::numeric_limits<SignedHalfWord>::max() ||
+               multWithOverflowCheck(lhs.num, static_cast<SignedHalfWord>(reduced_rhs), ans.num);
+    }
+
+    static bool multiply(const rat64_t& lhs, const long long& rhs, rat64_t& ans){
+        const auto gcd = std::gcd(lhs.den, safeAbs(rhs));
+        ans.den = lhs.den / gcd;
+        const auto reduced_rhs = rhs/gcd;
+        return reduced_rhs > std::numeric_limits<SignedHalfWord>::max() ||
+               reduced_rhs < std::numeric_limits<SignedHalfWord>::min() ||
+               multWithOverflowCheck(lhs.num, static_cast<SignedHalfWord>(reduced_rhs), ans.num);
+    }
+
+    static bool add(const SignedWord& ad, const SignedWord& bc, const UnsignedWord& bd, rat64_t& ans){
+        // a/b + c/d = (a*d + b*c)/(b*d)
+
+        if((ad >= 0) == (bc >= 0)){
+            //The addition result may not fit in a signed word, but will fit in an unsigned word
+            if(ad >= 0){
+                UnsignedWord ad_bc =
+                        static_cast<UnsignedWord>(ad)+static_cast<UnsignedWord>(bc);
+
+                const auto gcd = std::gcd(ad_bc, bd);
+                const auto den = bd / gcd;
+                const auto unsigned_num = ad_bc / gcd;
+
+                ans.num = unsigned_num;
+                ans.den = den;
+
+                return unsigned_num > std::numeric_limits<UnsignedHalfWord>::max() ||
+                       den > std::numeric_limits<SignedHalfWord>::max();
+            }else{
+                UnsignedWord ad_bc =
+                        static_cast<UnsignedWord>(-ad)+static_cast<UnsignedWord>(-bc);
+
+                const UnsignedWord gcd = std::gcd(ad_bc, bd);
+                const UnsignedWord den = bd / gcd;
+                const UnsignedWord unsigned_num = ad_bc / gcd;
+
+                ans.num = -unsigned_num;
+                ans.den = den;
+
+                return unsigned_num > std::numeric_limits<UnsignedHalfWord>::max() ||
+                       den > -(SignedWord)std::numeric_limits<SignedHalfWord>::min();
+            }
+        }else{
+            //The addition result will fit in a signed word
+            SignedWord ad_bc = ad + bc;
+
+            const UnsignedWord gcd = std::gcd(safeAbs(ad_bc), bd);
+
+            const SignedWord num = ad_bc / static_cast<SignedWord>(gcd);
+            const UnsignedWord den = bd / gcd;
+
+            ans.num = num;
+            ans.den = den;
+
+            return num > std::numeric_limits<SignedHalfWord>::max() ||
+                   num < std::numeric_limits<SignedHalfWord>::min() ||
+                   den > std::numeric_limits<UnsignedHalfWord>::max();
+        }
+    }
+
+    static bool add(const rat64_t& lhs, const rat64_t& rhs, rat64_t& ans){
+        // a/b + c/d = (a*d + b*c)/(b*d)
+
+        const SignedWord ad = static_cast<SignedWord>(rhs.num)*static_cast<SignedWord>(lhs.den);
+        const SignedWord bc = static_cast<SignedWord>(lhs.num)*static_cast<SignedWord>(rhs.den);
+        const UnsignedWord bd = static_cast<UnsignedWord>(lhs.den)*static_cast<UnsignedWord>(rhs.den);
+
+        return add(ad, bc, bd, ans);
+    }
+
+    static bool subtract(const rat64_t& lhs, const rat64_t& rhs, rat64_t& ans){
+        // a/b - c/d = (a*d - b*c)/(b*d)
+
+        UnsignedWord bd = static_cast<UnsignedWord>(lhs.den) * static_cast<UnsignedWord>(rhs.den);
+        SignedWord ad = static_cast<UnsignedWord>(rhs.num)*static_cast<UnsignedWord>(lhs.den);
+        SignedWord bc = -static_cast<UnsignedWord>(lhs.num)*static_cast<UnsignedWord>(rhs.den);
+
+        //Make sure to convert to larger type before negating, because
+        // -std::numeric_limits<SignedWord>::min() is UB
+
+        return add(ad, bc, bd, ans);
+    }
+
+    static bool add(const rat64_t& lhs, const SignedHalfWord& rhs, rat64_t& ans){
+        //The addition result will fit in a signed word
+        const SignedWord ad_bc = static_cast<SignedWord>(rhs)*static_cast<SignedWord>(lhs.den) + lhs.num;
+        const UnsignedWord gcd = std::gcd(safeAbs(ad_bc), lhs.den);
+        const SignedWord num = ad_bc / static_cast<SignedWord>(gcd);
+        const UnsignedWord den = lhs.den / gcd;
+
+        ans.num = num;
+        ans.den = den;
+
+        return num > std::numeric_limits<SignedHalfWord>::max() ||
+               num < std::numeric_limits<SignedHalfWord>::min() ||
+               den > std::numeric_limits<UnsignedHalfWord>::max();
+    }
+
+    static bool power(const rat64_t& lhs, const uint8_t& rhs, rat64_t& ans){
+        SignedWord num = 1;
+        UnsignedWord den = 1;
+
+        for(int i = 0; i < rhs; i++){
+            num *= lhs.num;
+            den *= lhs.den;
+            if(num > std::numeric_limits<SignedHalfWord>::max() ||
+               num < std::numeric_limits<SignedHalfWord>::min() ||
+               den > std::numeric_limits<UnsignedHalfWord>::max())
+                return true;
+        }
+
+        ans.num = num;
+        ans.den = den;
+        return false;
+    }
+
+    std::string toStr() const{
+        return std::to_string(num) + '/' + std::to_string(den);
+    }
+
+    friend std::ostream& operator<<(std::ostream& out, const rat64_t& rat){
+        out << std::to_string(rat.num) + '/' + std::to_string(rat.den);
+        return out;
+    }
+
+    bool operator==(const rat64_t& rhs) const{
+        assert(std::gcd(safeAbs(num), den) == 1);
+        assert(std::gcd(safeAbs(rhs.num), rhs.den) == 1);
+        return num == rhs.num && den == rhs.den;
+    }
+
+    bool operator!=(const rat64_t& rhs) const{
+        assert(std::gcd(safeAbs(num), den) == 1);
+        assert(std::gcd(safeAbs(rhs.num), rhs.den) == 1);
+        return num != rhs.num || den != rhs.den;
+    }
+
+    bool operator<(const rat64_t& rhs) const{
+        assert(std::gcd(safeAbs(num), den) == 1);
+        assert(std::gcd(safeAbs(rhs.num), rhs.den) == 1);
+        return
+            static_cast<SignedWord>(num)*static_cast<SignedWord>(rhs.den)
+            <
+            static_cast<SignedWord>(rhs.num)*static_cast<SignedWord>(den);
+    }
+
+    bool operator<=(const rat64_t& rhs) const{
+        assert(std::gcd(safeAbs(num), den) == 1);
+        assert(std::gcd(safeAbs(rhs.num), rhs.den) == 1);
+        return
+            static_cast<SignedWord>(num)*static_cast<SignedWord>(rhs.den)
+            <=
+            static_cast<SignedWord>(rhs.num)*static_cast<SignedWord>(den);
+    }
+
+    bool operator>(const rat64_t& rhs) const{
+        assert(std::gcd(safeAbs(num), den) == 1);
+        assert(std::gcd(safeAbs(rhs.num), rhs.den) == 1);
+        return
+            static_cast<SignedWord>(num)*static_cast<SignedWord>(rhs.den)
+            >
+            static_cast<SignedWord>(rhs.num)*static_cast<SignedWord>(den);
+    }
+
+    bool operator>=(const rat64_t& rhs) const{
+        assert(std::gcd(safeAbs(num), den) == 1);
+        assert(std::gcd(safeAbs(rhs.num), rhs.den) == 1);
+        return
+            static_cast<SignedWord>(num)*static_cast<SignedWord>(rhs.den)
+            >=
+            static_cast<SignedWord>(rhs.num)*static_cast<SignedWord>(den);
+    }
+
+    bool greaterThanPi() const{
+        //165707065/52746197 is the closest rat64_t to pi. It is greater by ~1.64084e-16.
+        //80143857/25510582 is the closest rat64_t less than pi. It is less by ~5.79087e-16
+        //Compare with the double approximation π-4*atan(1) with an error of ~3.4641e-07
+        return 52746197L*static_cast<SignedWord>(num) >= 165707065L*static_cast<SignedWord>(den);
+    }
+};
+
+#endif // RAT64_T_H
